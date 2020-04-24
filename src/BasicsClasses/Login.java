8a830e856c025553d785f97b738e2f39b0767719
Francisco Javier <fjavils200590@gmail.com>
2020-04-24 19:58:33 +0200

    Class Login not finished

--- src/BasicsClasses/Login.java
+++ src/BasicsClasses/Login.java
@@ -1,4 +1,43 @@
 package BasicsClasses;
 
 public class Login {
+
+    private String DNIEmployee;
+    private String password;
+
+    public Login() {
+        this.DNIEmployee = "Default";
+        this.password = "Default";
+    }
+
+    public Login(String DNIEmployee, String password) {
+        this.DNIEmployee = DNIEmployee;
+        this.password = password;
+    }
+
+    public Login(Login other) {
+        this.DNIEmployee = other.DNIEmployee;
+        this.password = other.password;
+    }
+
+    public String getDNIEmployee() {
+        return DNIEmployee;
+    }
+
+    public void setDNIEmployee(String DNIEmployee) {
+        this.DNIEmployee = DNIEmployee;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return DNIEmployee+","+password;
+    }
 }
