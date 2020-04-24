aa32fc31dbc1f54230a984a98de44607997f1d8b
Francisco Javier <fjavils200590@gmail.com>
2020-04-24 19:55:36 +0200

    Class Order finished

--- src/BasicsClasses/Order.java
+++ src/BasicsClasses/Order.java
@@ -1,4 +1,63 @@
 package BasicsClasses;
 
+import java.util.GregorianCalendar;
+
 public class Order {
+
+    private int IDProduct;
+    private int amountProduct;
+    private GregorianCalendar dateOrder;
+
+
+    public Order(){
+        IDProduct = -1;
+        amountProduct = -1;
+        dateOrder = new GregorianCalendar();
+    }
+
+    public Order(int IDProduct, int amountProduct) {
+        this.IDProduct = IDProduct;
+        this.amountProduct = amountProduct;
+        this.dateOrder = new GregorianCalendar();
+    }
+
+    public Order(int IDProduct, int amountProduct, GregorianCalendar dateOrder) {
+        this.IDProduct = IDProduct;
+        this.amountProduct = amountProduct;
+        this.dateOrder = dateOrder;
+    }
+
+
+    public Order(Order other) {
+        this.IDProduct = other.IDProduct;
+        this.amountProduct = other.amountProduct;
+        this.dateOrder = other.dateOrder;
+    }
+
+    public int getIDProduct() {
+        return IDProduct;
+    }
+
+    public void setIDProduct(int IDProduct) {
+        this.IDProduct = IDProduct;
+    }
+
+    public int getAmountProduct() {
+        return amountProduct;
+    }
+
+    public void setAmountProduct(int amountProduct) {
+        this.amountProduct = amountProduct;
+    }
+
+    public GregorianCalendar getDateOrder() {
+        return new GregorianCalendar(dateOrder.DAY_OF_MONTH,dateOrder.MONTH,dateOrder.YEAR);
+    }
+
+    @Override
+    public String toString(){
+        return IDProduct+"|"+amountProduct+"|"+dateOrder.DAY_OF_MONTH+"|"+(dateOrder.MONTH+1)+"|"+dateOrder.YEAR;
+    }
+
+
 }
