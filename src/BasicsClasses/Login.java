8289595ff4ae08079b230a9b0be2f67c70654e4a
Francisco Javier <fjavils200590@gmail.com>
2020-04-25 19:46:06 +0200

    Interface ILogin and class Login finished

--- src/BasicsClasses/Login.java
+++ src/BasicsClasses/Login.java
@@ -1,43 +1,107 @@
 package BasicsClasses;
 
-public class Login {
+/*
+ * Properties:
+ *  - Basics:
+ *      > username: integer, Consulting
+ *      > password: integer, Consulting
+ *
+ *  - Derivatives:
+ *      > None
+ *
+ *  - Shared:
+ *      > None
+ *
+ * Methods:
+ *  - Basics:
+ *      > string getUsername()
+ *
+ *      > string getPassword()
+ *
+ *  - Added:
+ *      > None
+ *
+ */
 
-    private String DNIEmployee;
+import Interfaces.ILogin;
+
+public class Login implements ILogin,Cloneable,Comparable {
+
+    private String username;
     private String password;
 
     public Login() {
-        this.DNIEmployee = "Default";
+        this.username = "Default";
         this.password = "Default";
     }
 
     public Login(String DNIEmployee, String password) {
-        this.DNIEmployee = DNIEmployee;
+        this.username = DNIEmployee;
         this.password = password;
     }
 
     public Login(Login other) {
-        this.DNIEmployee = other.DNIEmployee;
+        this.username = other.username;
         this.password = other.password;
     }
 
-    public String getDNIEmployee() {
-        return DNIEmployee;
-    }
-
-    public void setDNIEmployee(String DNIEmployee) {
-        this.DNIEmployee = DNIEmployee;
+    public String getUsername() {
+        return username;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    @Override
+    public String toString() {
+        return username+"|"+password;
     }
 
     @Override
-    public String toString() {
-        return DNIEmployee+","+password;
+    public int compareTo(Object ob){
+        int result = -1;
+
+        Login otherLogin = (Login)ob;
+
+        if (this.username.compareTo(otherLogin.username) == 0){
+            result = 0;
+        }else{
+            if (this.username.compareTo(otherLogin.username) > 0){
+                result = 1;
+            }
+        }
+        return result;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        boolean isEquals = false;
+        if (this == obj){
+            isEquals = true;
+        }else{
+            if (obj != null && obj instanceof Login){
+                Login newLogin = (Login)obj;
+                if (this.username.equals(newLogin.username)
+                    && this.password.equals(newLogin.password)){
+                    isEquals = true;
+                }
+            }
+        }
+        return isEquals;
+    }
+
+    @Override
+    public Login clone() {
+        Login newLogin = null;
+
+        try {
+            newLogin = (Login)super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+
+        return newLogin;
+    }
+
 }
