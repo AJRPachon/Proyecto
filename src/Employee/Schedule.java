411157e9ad7e23c3a078141010a69a70d378d83d
AJRPachon <49155621+AJRPachon@users.noreply.github.com>
2020-04-25 11:23:26 +0200

    estudios de clases

---
+++ src/Employee/Schedule.java
@@ -0,0 +1,65 @@
+package Employee;
+
+/*
+
+    ANALYSIS:
+        Collect the schedule of each employee ( shift, day of the week and time at which the service must be performed )
+
+    BASIC PROPERTIES:
+        weekDay enum consulting and modifiable
+        time GregorianCalendar consulting and modifiable
+
+    DERIVATE PROPERTIES:
+        shift String consulting ( Depends on the time )
+
+
+    INTERFACE
+    BASIC METHODS:
+        getWeekDay();
+        setWeekDay(enum);
+
+        getTime();
+        setTime(GregorianCalendar hour);
+
+        getShift();
+
+
+    ADDED METHODS:
+        consultShedule() ( Displays the time, day and shift on the screen )
+
+ */
+
+
+public class Schedule {
+
+
+/////// BASIC METHODS ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+/////// ADDED METHODS ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////// CONSULT SHEDULE ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /*
+        SIGNATURE: void consultSchedule()
+
+        COMENTARY: Shows the time, shift and day of the week
+
+        INPUTS: Not one
+
+        OUTPUTS: Not one
+
+        INTPUT/OUTPUT: Not one
+
+        PRECONDITION: Attribute values must be !null or >= 0
+
+        POSTCONDITION: Values printed per screen
+
+     */
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+}
---
+++ src/FoodstuffDrinks/Consumable.java
@@ -0,0 +1,4 @@
+package FoodstuffDrinks;
+
+public class Consumable {
+}
