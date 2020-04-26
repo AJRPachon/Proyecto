c3777ed5595e4277cb7fd6717f3a237b1cfb75af
Francisco Javier <fjavils200590@gmail.com>
2020-04-26 19:18:31 +0200

    All methods of interface IOrder implemented in class Order

--- src/BasicsClasses/Order.java
+++ src/BasicsClasses/Order.java
@@ -21,8 +21,14 @@ package BasicsClasses;
  *      > integer getAmountProduct()
  *      > none setAmountProduct(integer amountProduct)
  *
- *      > date getDateOrder()
- *      > none setDateOrder(date dateOrder)
+ *      > date getDateOrder();
+ *          > integer getDayOfDateOrder();
+ *          > integer getMonthOfDateOrder();
+ *          > integer getYearOfDateOrder();
+ *      > none setDateOrder(date dateOrder);
+ *          > none setDayOfDateOrder(integer day);
+ *          > none setMonthOfDateOrder(integer month);
+ *          > none setYearOfDateOrder(integer year);
  *
  *  - Added:
  *      > None
@@ -32,6 +38,7 @@ package BasicsClasses;
 
 import Interfaces.IOrder;
 
+import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class Order implements IOrder,Cloneable,Comparable {
@@ -58,12 +65,12 @@ public class Order implements IOrder,Cloneable,Comparable {
         this.dateOrder = dateOrder;
     }
 
-
     public Order(Order other) {
         this.IDProduct = other.IDProduct;
         this.amountProduct = other.amountProduct;
         this.dateOrder = other.dateOrder;
     }
+
 
     public int getIDProduct() {
         return IDProduct;
@@ -82,16 +89,39 @@ public class Order implements IOrder,Cloneable,Comparable {
     }
 
     public GregorianCalendar getDateOrder() {
-        return new GregorianCalendar(dateOrder.DAY_OF_MONTH,dateOrder.MONTH,dateOrder.YEAR);
+        return new GregorianCalendar(dateOrder.get(Calendar.DAY_OF_MONTH),dateOrder.get(Calendar.MONTH),dateOrder.get(Calendar.YEAR));
+    }
+    public int getDayOfDateOrder(){
+        return dateOrder.get(Calendar.DAY_OF_MONTH);
+    }
+
+    public int getMonthOfDateOrder(){
+        return dateOrder.get(Calendar.MONTH);
+    }
+
+    public int getYearOfDateOrder() {
+        return dateOrder.get(Calendar.YEAR);
     }
 
     public void setDateOrder(GregorianCalendar dateOrder){
-        this.dateOrder = new GregorianCalendar(dateOrder.DAY_OF_MONTH,dateOrder.MONTH,dateOrder.YEAR);
+        this.dateOrder = new GregorianCalendar(dateOrder.get(Calendar.DAY_OF_MONTH),dateOrder.get(Calendar.MONTH),dateOrder.get(Calendar.YEAR));
+    }
+
+    public void setDayOfDateOrder(int day){
+        dateOrder.set(Calendar.DAY_OF_MONTH,day);
+    }
+
+    public void setMonthOfDateOrder(int month){
+        dateOrder.set(Calendar.MONTH,month);
+    }
+
+    public void setYearOfDateOrder(int year){
+        dateOrder.set(Calendar.YEAR,year);
     }
 
     @Override
     public String toString(){
-        return IDProduct+"|"+amountProduct+"|"+dateOrder.DAY_OF_MONTH+"|"+(dateOrder.MONTH+1)+"|"+dateOrder.YEAR;
+        return IDProduct+"|"+amountProduct+"|"+dateOrder.get(Calendar.DAY_OF_MONTH)+"|"+(dateOrder.get(Calendar.MONTH)+1)+"|"+dateOrder.get(Calendar.YEAR);
     }
 
     @Override
