6f46da5e7221f7036d45bc3967d7e5cf42499655
AJRPachon <49155621+AJRPachon@users.noreply.github.com>
2020-05-02 18:18:44 +0200

    ToString Employee fixed

--- src/Management/FilesManagement.java
+++ src/Management/FilesManagement.java
@@ -1,5 +1,9 @@
 package Management;
 
+import BasicsClasses.Employee.Enums.EnumWeekDays;
+
+import java.util.GregorianCalendar;
+
 public class FilesManagement {
 
 
@@ -7,22 +11,35 @@ public class FilesManagement {
 
 /*
 * SIGNATURE:
+*   public void assignSchedule(int IDEmployee, GregorianCalendar day, GregorianCalendar hour, GregorianCalendar minute, EnumWeekDays weekDay)
 *
 * COMENTARY:
 *   This method must be able to assign a specific employee a schedule
 *
 * INPUT:
-*   IDEmployee, GregorianCalendar day, G
+*   integer IDEmployee, GregorianCalendar day, GregorianCalendar hour, GregorianCalendar minute, EnumWeekDays weekDay
 *
 * OUTPUT:
+*   Not one
 *
 * INPUT/OUTPUT:
+*   Not one
 *
 * PRECONDITION:
+*
 *
 * POSTCONDITION:
 *
 *
  */
+
+    public void assignSchedule(int IDEmployee, GregorianCalendar day, GregorianCalendar hour, GregorianCalendar minute, EnumWeekDays weekDay){
+
+
+
+
+    }
+
+
 
 }
