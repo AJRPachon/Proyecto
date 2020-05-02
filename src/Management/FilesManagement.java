2d7c076eb90c4f536d8e61287a59be77ef34cb66
AJRPachon <49155621+AJRPachon@users.noreply.github.com>
2020-05-02 17:20:24 +0200

    Added methods: get/set hour and minute

--- src/Management/FilesManagement.java
+++ src/Management/FilesManagement.java
@@ -3,5 +3,26 @@ package Management;
 public class FilesManagement {
 
 
+///// ASSIGN SCHEDULE //////////////////////
+
+/*
+* SIGNATURE:
+*
+* COMENTARY:
+*   This method must be able to assign a specific employee a schedule
+*
+* INPUT:
+*   IDEmployee, GregorianCalendar day, G
+*
+* OUTPUT:
+*
+* INPUT/OUTPUT:
+*
+* PRECONDITION:
+*
+* POSTCONDITION:
+*
+*
+ */
 
 }
