7df270afcd2c5fd555271145be0381c3a61fed53
AJRPachon <49155621+AJRPachon@users.noreply.github.com>
2020-05-02 16:22:10 +0200

    test finished

--- src/Management/FilesManagement.java
+++ src/Management/FilesManagement.java
@@ -2,11 +2,5 @@ package Management;
 
 public class FilesManagement {
 
-    public void holaFran(){
-
-        System.out.println("Me cago en dios Fran");
-    }
-
-    //A ver si esto funciona
 
 }
