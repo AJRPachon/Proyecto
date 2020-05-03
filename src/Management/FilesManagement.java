a08695eda67d6a212288b679437199088b9adf76
AJRPachon <49155621+AJRPachon@users.noreply.github.com>
2020-05-03 12:45:15 +0200

    Create scheduleManagement for Schedule methods

--- src/Management/FilesManagement.java
+++ src/Management/FilesManagement.java
@@ -4,44 +4,11 @@ import BasicsClasses.Employee.Employee;
 import BasicsClasses.Employee.Enums.EnumWeekDays;
 import BasicsClasses.Employee.Schedule;
 
-import java.util.GregorianCalendar;
-import java.util.Scanner;
+
 
 public class FilesManagement {
 
 
-///// ASSIGN SCHEDULE //////////////////////
-
-/*
-* SIGNATURE:
-*   public void assignSchedule(int IDEmployee, GregorianCalendar day, GregorianCalendar hour, GregorianCalendar minute, EnumWeekDays weekDay)
-*
-* COMENTARY:
-*   This method must be able to assign a specific employee a schedule
-*
-* INPUT:
-*   integer IDEmployee, GregorianCalendar day, GregorianCalendar hour, GregorianCalendar minute, EnumWeekDays weekDay
-*
-* OUTPUT:
-*   Not one
-*
-* INPUT/OUTPUT:
-*   Not one
-*
-* PRECONDITION:
-*
-*
-* POSTCONDITION:
-*
-*
- */
-
-    public void assignSchedule(Employee employee, GregorianCalendar startDate, GregorianCalendar endDate, EnumWeekDays weekDay){
-
-        
-
-
-    }
 
 
 
