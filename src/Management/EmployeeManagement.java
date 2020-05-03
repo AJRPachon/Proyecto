d7e479be65a7cae2de7361e17a8d1eabececabab
Francisco Javier <fjavils200590@gmail.com>
2020-05-03 17:22:53 +0200

    Class EmployeeManagement created and method getPermisons too

---
+++ src/Management/EmployeeManagement.java
@@ -0,0 +1,51 @@
+package Management;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class EmployeeManagement {
+
+    /**
+     * This method check if the username and the password passed by parameter exist and return the permisons of this employee.
+     * If the username not exist or the password are wrong, return null.
+     * @param username Username of employee
+     * @param password Password of employee
+     * @return Return the permisons of this employee. If the username not exist or the password are wrong, return null.
+     */
+
+    public String getPermisons(String username, String password){
+
+        String category = null;
+        BufferedReader BR = null;
+        String line;
+        String[] lineDivide;
+
+        try {
+            BR = new BufferedReader(new FileReader(".\\src\\Files\\Employees"));
+            line = BR.readLine();
+            while (line != null && category == null){
+                lineDivide = line.split("&");
+                if (lineDivide[2].equals(username) && lineDivide[8].equals(password)){
+                    category = lineDivide[6];
+                }
+                line = BR.readLine();
+            }
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                BR.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return category;
+    }
+
+}
