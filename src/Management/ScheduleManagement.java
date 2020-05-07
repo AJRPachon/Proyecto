2a8bb6184ee81200df7ec4b1b100766b23ac2be5
AJRPachon <49155621+AJRPachon@users.noreply.github.com>
2020-05-07 12:57:38 +0200

    Refactor assign schedule to ScheduleManagement

--- src/Management/ScheduleManagement.java
+++ src/Management/ScheduleManagement.java
@@ -8,34 +8,86 @@ public class ScheduleManagement {
 
     ///// ASSIGN SCHEDULE //////////////////////
 
-    /**
+
+    /*
+     *   SIGNATURE:
+     *       public void assignSchedule()
      *
+     *   COMENTARY:
+     *       This method assigns a schedule to an employee
+     *
+     *   INPUTS:
+     *       Not one
+     *
+     *   OUTPUTS:
+     *       Not one
+     *
+     *   INPUT/OUTPUTS:
+     *       Not one
+     *
+     *   PRECONDITION:
+     *       Not one
+     *
+     *   POSTCONDITION:
+     *       The schedule must have been assigned
      *
      */
 
     public void assignSchedule(Schedule schedule){
 
-        Scanner teclado = new Scanner(System.in);
+        Scanner kb = new Scanner(System.in);
 
-        int SDHour;
+        int SDHour;  //SD = START DATE
         int SDMinute;
-        int EDHour;
+        int EDHour;  //ED = END DATE
         int EDMinute;
 
-        System.out.println("Start time of the day");
-        SDHour = teclado.nextInt();
+
+        //SD HOUR
+        System.out.println("Enter the start time of the day");
+        SDHour = kb.nextInt();
+
+        //Validation
+        while( SDHour < 0 || SDHour > 24 ){
+            System.out.println("The time must be between 0 and 24");
+            SDHour = kb.nextInt();
+        }
         schedule.setSDHour(SDHour);
 
-        System.out.println("minutes start of the day");
-        SDMinute = teclado.nextInt();
+
+        //SD MINUTE
+        System.out.println("Enter minute of start of the day");
+        SDMinute = kb.nextInt();
+
+        //Validation
+        while( SDMinute < 0 || SDMinute > 59 ){
+            System.out.println("The minutes must be between 0 and 59");
+            SDMinute = kb.nextInt();
+        }
         schedule.setSDMinutes(SDMinute);
 
-        System.out.println("Hora fin de la jornada");
-        EDHour = teclado.nextInt();
+
+        //ED HOUR
+        System.out.println("Enter end-of-day hour");
+        EDHour = kb.nextInt();
+
+        //Validation
+        while( EDHour < 0 || EDHour > 24 ){
+            System.out.println("The time must be between 0 and 24");
+            EDHour = kb.nextInt();
+        }
         schedule.setEDHour(EDHour);
 
-        System.out.println("Minutos fin de la jornada");
-        EDMinute = teclado.nextInt();
+
+        //ED MINUTE
+        System.out.println("Enter end-of-day minute");
+        EDMinute = kb.nextInt();
+
+        //Validation
+        while( EDMinute < 0 || EDMinute > 59 ){
+            System.out.println("The minutes must be between 0 and 59");
+            EDMinute = kb.nextInt();
+        }
         schedule.setEDMinutes(EDMinute);
 
     }
