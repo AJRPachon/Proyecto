0761125ad6ee7d014d2cacba3a0695411acc5717
AJRPachon <49155621+AJRPachon@users.noreply.github.com>
2020-05-08 09:11:17 +0200

    Deleted assign schedule from FilesManagement

--- src/Management/FilesManagement.java
+++ src/Management/FilesManagement.java
@@ -78,38 +78,7 @@ public class FilesManagement {
 
         }
     }
-
-
-///// ASSIGN SCHEDULE //////////////////////
-
-/*
-* SIGNATURE:
-*   public void assignSchedule(int IDEmployee, GregorianCalendar day, GregorianCalendar hour, GregorianCalendar minute, EnumWeekDays weekDay)
-*
-* COMENTARY:
-*   This method must be able to assign a specific employee a schedule
-*
-* INPUT:
-*   integer IDEmployee, GregorianCalendar day, GregorianCalendar hour, GregorianCalendar minute, EnumWeekDays weekDay
-*
-* OUTPUT:
-*   Not one
-*
-* INPUT/OUTPUT:
-*   Not one
-*
-* PRECONDITION:
-*
-*
-* POSTCONDITION:
-*
-*
- */
-
-    public void assignSchedule(int IDEmployee, GregorianCalendar day, GregorianCalendar hour, GregorianCalendar minute, EnumWeekDays weekDay){
-
-
-    }
+    
 
 
 }
