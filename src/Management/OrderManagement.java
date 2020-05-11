dded4625ed26b966a6dd0e4f171292bc1aa1b3eb
Francisco Javier <fjavils200590@gmail.com>
2020-05-11 23:36:03 +0200

    Some bugs in some methods fixed and added method readAndValidateNewOrder

--- src/Management/OrderManagement.java
+++ src/Management/OrderManagement.java
@@ -1,41 +1,45 @@
 package Management;
 
+import BasicsClasses.FoodstuffDrinks.Product;
 import BasicsClasses.Orders.Order;
+import BasicsClasses.Orders.OrderLine;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
 
 public class OrderManagement {
 
     /**
-     * @param order
-     * @return
+     * Read and validate the data to create an order object.
+     * There must be at least 1 product in file product.
+     * @return Return object Order valided
      */
 
-    public boolean insertOrder(Order order){    //TODO Colsuntar si esto esta bien
-        boolean orderInserted = false;
-        FileWriter FW = null;
-        try {
-            FW = new FileWriter(".\\src\\Files\\tmp\\OrdersTemp",true);
-            FW.write(order.toString());
-            FW.flush();
-            orderInserted = true;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        finally {
-            try {
-                FW.close();
-            }catch (IOException|NullPointerException error){
-                error.printStackTrace();
-            }
-        }
-        return orderInserted;
-    }
-
-
     public Order readAndValidateNewOrder(){
-        Order newOrder = null;
+        Scanner sc = new Scanner(System.in);
+        FilesManagement FM = new FilesManagement();
+        Validations Val = new Validations();
+
+        int quantity;
+        char character;
+        Order newOrder = new Order(new GregorianCalendar());
+
+        Product productGet;
+
+        do {
+
+            productGet = FM.readAndSearchProduct();
+            quantity = Val.readAndValidateQuantityOfProduct();
+            newOrder.addOrderLine(new OrderLine(productGet,quantity));
+
+            //Read to want insert more line product
+            do {
+                System.out.print("Insert S or N: ");
+                character = Character.toUpperCase(sc.next().charAt(0));
+            }while (character != 'N' && character != 'S');
+
+        }while (character == 'S');
+
         return newOrder;
     }
 
