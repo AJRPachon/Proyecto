971944db6dae0ec85e289991de63e47abc4a5d4b
AJRPachon <49155621+AJRPachon@users.noreply.github.com>
2020-06-01 16:56:48 +0200

    payslip obj employee

--- src/BasicsClasses/Employee/Payslip.java
+++ src/BasicsClasses/Employee/Payslip.java
@@ -29,17 +29,19 @@ package BasicsClasses.Employee;
 public class Payslip {
 
     private double salary;
-    //TODO obj empleado
+    private Employee employee;
 
     public Payslip(){ //Constructor without parameters
 
         this.salary = 0;
+        this.employee = new Employee();
 
     }
 
-    public Payslip(double salary){ //Constructor with parameters
+    public Payslip(double salary, Employee employee){ //Constructor with parameters
 
         this.salary = salary;
+        this.employee = employee;
 
     }
 
@@ -53,7 +55,16 @@ public class Payslip {
         this.salary = salary;
     }
 
-/////// TO STRING ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public Employee getEmployee() {
+        return this.employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    /////// TO STRING ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     @Override
     public String toString() {
