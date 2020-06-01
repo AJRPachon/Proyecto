74b51abea272bd7c0b6b3d5010b42a300846cfad
Francisco Javier Avilés Fernández <fjavils200590@gmail.com>
2020-06-01 18:44:23 +0200

    Revert "Wwf antonio"

--- src/BasicsClasses/Employee/Payslip.java
+++ src/BasicsClasses/Employee/Payslip.java
@@ -29,19 +29,16 @@ package BasicsClasses.Employee;
 public class Payslip {
 
     private double salary;
-    private Employee employee;
 
     public Payslip(){ //Constructor without parameters
 
         this.salary = 0;
-        this.employee = new Employee();
 
     }
 
-    public Payslip(double salary, Employee employee){ //Constructor with parameters
+    public Payslip(double salary){ //Constructor with parameters
 
         this.salary = salary;
-        this.employee = employee;
 
     }
 
@@ -55,20 +52,11 @@ public class Payslip {
         this.salary = salary;
     }
 
-
-    public Employee getEmployee() {
-        return this.employee;
-    }
-
-    public void setEmployee(Employee employee) {
-        this.employee = employee;
-    }
-
-    /////// TO STRING ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////// TO STRING ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     @Override
     public String toString() {
-        return this.salary+"#";
+        return "El salario es: "+this.salary+"€";
     }
 
 
