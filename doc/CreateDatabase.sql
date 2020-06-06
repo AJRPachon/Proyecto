8e1e43ca9bf768a6271c2bc5951a99923925f72d
Francisco Javier <fjavils200590@gmail.com>
2020-06-06 14:25:31 +0200

    Script create database finished

--- doc/CreateDatabase.sql
+++ doc/CreateDatabase.sql
@@ -1,6 +1,9 @@
 CREATE DATABASE Restaurant
+GO
+
 
 USE Restaurant
+GO
 
 CREATE TABLE Employees(
 	DNI CHAR(9) NOT NULL,
@@ -68,65 +71,79 @@ CREATE TABLE OrdersLines(
 )
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-CREATE TABLE Foods(
+CREATE TABLE ConsumablesFoods(
 	ID INT IDENTITY NOT NULL,
 	[Name] VARCHAR(20) NOT NULL,
 	[Description] VARCHAR(30) NOT NULL,
 	Price MONEY NULL,
-	[Type] VARCHAR(9) NULL
+	[Type] VARCHAR(9) NULL,
+	CONSTRAINT PK_ConsumablesFoods PRIMARY KEY (ID)
 )
 
 
-CREATE TABLE Drinks(
+CREATE TABLE ConsumablesDrinks(
 	ID INT IDENTITY NOT NULL,
 	[Name] VARCHAR(20) NOT NULL,
 	[Description] VARCHAR(30) NOT NULL,
 	Price MONEY NULL,
 	Proof DECIMAL(2,1) NULL DEFAULT (0.0),
-	Sparkling BIT NULL DEFAULT (0)
+	Sparkling BIT NULL DEFAULT (0),
+	CONSTRAINT PK_ConsumablesDrinks PRIMARY KEY (ID)
 )
 
 
-CREATE TABLE Allergies(
+CREATE TABLE AllergiesConsumables(
 	ID INT IDENTITY NOT NULL,
-	[Name] VARCHAR(12) NOT NULL
+	[Name] VARCHAR(12) NOT NULL,
+	CONSTRAINT PK_Allergies PRIMARY KEY (ID)
+)
+
+
+CREATE TABLE AllergiesConsumablesFoods(
+	ID INT IDENTITY NOT NULL,
+	IDConsumableFood INT NOT NULL,
+	IDAllergieConsumable INT NOT NULL,
+	CONSTRAINT PK_AllergiesConsumablesFoods PRIMARY KEY (ID),
+	CONSTRAINT FK_AllergiesConsumablesFoods_IDConsumableFood FOREIGN KEY (IDConsumableFood) REFERENCES ConsumablesFoods(ID) ON DELETE NO ACTION ON UPDATE NO ACTION,
+	CONSTRAINT FK_AllergiesConsumablesFoods_IDAllergiesConsumables FOREIGN KEY (IDAllergieConsumable) REFERENCES AllergiesConsumables(ID) ON DELETE NO ACTION ON UPDATE NO ACTION
+)
+
+
+CREATE TABLE AllergiesConsumablesDrinks(
+	ID INT IDENTITY NOT NULL,
+	IDConsumableDrink INT NOT NULL,
+	IDAllergieConsumable INT NOT NULL,
+	CONSTRAINT PK_AllergiesConsumablesDrinks PRIMARY KEY (ID),
+	CONSTRAINT FK_AllergiesConsumablesDrinks_IDConsumableDrink FOREIGN KEY (IDConsumableDrink) REFERENCES ConsumablesDrinks(ID) ON DELETE NO ACTION ON UPDATE NO ACTION,
+	CONSTRAINT FK_AllergiesConsumablesDrinks_IDAllergieConsumable FOREIGN KEY (IDAllergieConsumable) REFERENCES AllergiesConsumables(ID) ON DELETE NO ACTION ON UPDATE NO ACTION
 )
 
 
 CREATE TABLE Receipts(
 	ID INT IDENTITY NOT NULL,
 	DateOfReceipt SMALLDATETIME NOT NULL,
-	PaidFor BIT NULL DEFAULT (0)
+	PaidFor BIT NULL DEFAULT (0),
+	CONSTRAINT PK_Receipts PRIMARY KEY (ID)
 )
 
 
-CREATE TABLE ReceiptsLines(
+CREATE TABLE ReceiptsLinesConsumablesDrinks(
 	ID INT IDENTITY NOT NULL,
 	IDReceipt INT NOT NULL,
-	IDConsumable INT NOT NULL,
-	Quantity INT NOT NULL
+	IDConsumableDrink INT NOT NULL,
+	Quantity INT NOT NULL,
+	CONSTRAINT PK_ReceiptsLinesConsumablesDrinks PRIMARY KEY (ID),
+	CONSTRAINT FK_ReceiptsLinesConsumablesDrinks_IDReceipt FOREIGN KEY (IDReceipt) REFERENCES Receipts(ID) ON DELETE NO ACTION ON UPDATE NO ACTION,
+	CONSTRAINT FK_ReceiptsLinesConsumablesDrinks_IDConsumableDrink FOREIGN KEY (IDConsumableDrink) REFERENCES ConsumablesDrinks(ID) ON DELETE NO ACTION ON UPDATE NO ACTION
 )
 
 
-
-
-
-
-
-
+CREATE TABLE ReceiptsLinesConsumablesFoods(
+	ID INT IDENTITY NOT NULL,
+	IDReceipt INT NOT NULL,
+	IDConsumableFood INT NOT NULL,
+	Quantity INT NOT NULL,
+	CONSTRAINT PK_ReceiptsLinesConsumablesFoods PRIMARY KEY (ID),
+	CONSTRAINT FK_ReceiptsLinesConsumablesFoods_IDReceipt FOREIGN KEY (IDReceipt) REFERENCES Receipts(ID) ON DELETE NO ACTION ON UPDATE NO ACTION,
+	CONSTRAINT FK_ReceiptsLinesConsumablesFoods_IDConsumableFood FOREIGN KEY (IDConsumableFood) REFERENCES ConsumablesFoods(ID) ON DELETE NO ACTION ON UPDATE NO ACTION
+)
