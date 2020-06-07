7c35df717ccade98b69698d30bec76a1297afefe
AJRPachon <49155621+AJRPachon@users.noreply.github.com>
2020-06-07 13:01:37 +0200

    Update EnumPosition.java

--- src/BasicsClasses/Employee/Enums/EnumPosition.java
+++ src/BasicsClasses/Employee/Enums/EnumPosition.java
@@ -2,23 +2,5 @@ package BasicsClasses.Employee.Enums;
 
 public enum EnumPosition {
     CommisChef, HeadChef, ChefThePartie, SousChef, Cook, Waiter, Waitress, Busser, Manager, Host, Bartender, Spaguetti;
-
-
-    public EnumPosition getEmumPosition(String position){
-
-        EnumPosition e = Spaguetti;
-
-        for (int cont = 0; cont < EnumPosition.values().length; cont++){
-
-            if ( position.equals(EnumPosition.values()[cont].toString()) ){
-
-                e = EnumPosition.values()[cont];
-
-            }
-
-        }
-
-        return e;
-
-    }
+    
 }
