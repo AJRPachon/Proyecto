e2819b231b07fbded6e8fce88a9478f3ef812239
AJRPachon <49155621+AJRPachon@users.noreply.github.com>
2020-06-08 13:42:30 +0200

    enum test

---
+++ src/JUnitTest/employee/Enums/EnumWeekDaysTest.java
@@ -0,0 +1,20 @@
+package JUnitTest.employee.Enums;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnumWeekDaysTest {
+
+    @Test
+    void getWeekDay() {
+    }
+
+    @Test
+    void values() {
+    }
+
+    @Test
+    void valueOf() {
+    }
+}
