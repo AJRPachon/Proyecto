473328512f3be6bfcc8bc9a3d2d6b5c8b29490c6
Francisco Javier <fjavils200590@gmail.com>
2020-06-08 15:46:08 +0200

    A lot of methods added

--- src/Management/DatabaseManagement.java
+++ src/Management/DatabaseManagement.java
@@ -3,10 +3,14 @@ package Management;
 import BasicsClasses.Employee.Employee;
 import BasicsClasses.Employee.Enums.EnumCategory;
 import BasicsClasses.Employee.Enums.EnumPosition;
+import BasicsClasses.Employee.Enums.EnumWeekDays;
 import BasicsClasses.Employee.Payslip;
 import BasicsClasses.Employee.Schedule;
+import BasicsClasses.FoodstuffDrinks.Product;
 import BasicsClasses.Orders.Order;
 import BasicsClasses.Orders.OrderLine;
+
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -14,6 +18,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+
 
 
 public class DatabaseManagement {
@@ -71,14 +76,70 @@ public class DatabaseManagement {
         boolean succes = false;
         Statement sentence = null;
 
-        String select = "INSERT INTO Employees (DNI, Password, Name, Surname, NAF, Birthday, Position, Category, BankAccount, Active)" +
+        String insert = "INSERT INTO Employees (DNI, Password, Name, Surname, NAF, Birthday, Position, Category, BankAccount, Active)" +
                 "values ('"+employee.getDNI()+"', '"+employee.getPassword()+"', '"+employee.getName()+"', '"+employee.getSurname()+"', '"+employee.getnAF()+"', " +
                 "'"+employee.getBirthday().get(Calendar.MONTH)+"/"+employee.getBirthday().get(Calendar.DAY_OF_MONTH)+"/"+employee.getBirthday().get(Calendar.YEAR)+"', " +
                 "'"+employee.getPosition()+"', '"+employee.getCategory()+"', '"+employee.getBankAccountN()+"',1)";
 
         try {
             sentence = connection.createStatement();
-            succes = (sentence.executeUpdate(select) > 0);
+            succes = (sentence.executeUpdate(insert) > 0);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        try {
+            sentence.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return succes;
+    }
+
+
+    /**
+     * @param order
+     * @param connection
+     * @return
+     */
+
+
+    public boolean insertNewOrder(String DNIEmployee, Order order, Connection connection){
+
+        boolean succes = false;
+        Statement sentence = null;
+        ResultSet ID;
+        int IDOrder = 0;
+
+        String dateOrder = order.getMonthOfDateOrder()+"-"+order.getDayOfDateOrder()+"-"+order.getYearOfDateOrder();
+
+        String insert = "INSERT INTO Orders (DNIEmployee, DateOrder, Sent, Cancel) " +
+                        "VALUES ('"+DNIEmployee+"','"+dateOrder+"',0,0)";
+
+        try {
+            sentence = connection.createStatement();
+            succes = (sentence.executeUpdate(insert) > 0);
+            ID = sentence.executeQuery("SELECT @@IDENTITY AS ID");
+            while (ID.next()){
+                IDOrder = ID.getInt("ID");
+            }
+            if (succes){
+
+                insert = "INSERT INTO OrdersLines(IDOrder, IDProduct, Quantity) VALUES ";
+
+                for (int i = 0; i < order.getOrdersLines().size(); i++){
+
+                    if (i == order.getOrdersLines().size()-1){
+                        insert+= " ("+IDOrder+","+order.getOrderLineIndex(i).getIDProduct()+","+order.getOrderLineIndex(i).getProductQuantity()+")";
+                    }else{
+                        insert+= " ("+IDOrder+","+order.getOrderLineIndex(i).getIDProduct()+","+order.getOrderLineIndex(i).getProductQuantity()+"), ";
+                    }
+                }
+
+                succes = (sentence.executeUpdate(insert) > 0);
+
+            }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
@@ -144,9 +205,11 @@ public class DatabaseManagement {
         int day, month, year;
         boolean sent, cancel;
         Order orderRead;
+        ArrayList<OrderLine> ordersLines;
 
         String select = "SELECT ID,DateOrder,[Sent],Cancel " +
-                        "FROM Orders";
+                        "FROM Orders " +
+                        "WHERE Sent = 0 AND Cancel = 0";
 
         try {
             sentence = connection.createStatement();
@@ -161,11 +224,11 @@ public class DatabaseManagement {
                 sent = orders.getBoolean("Sent");
                 cancel = orders.getBoolean("Cancel");
 
-                orderRead = new Order(orders.getInt("ID"),date,sent,cancel);
+                ordersLines = getOrderLineByIDOrder(orders.getInt("ID"), connection);
 
-                if (!orderRead.getSent() && !orderRead.getCancel()){
-                    ordersNotShipped.add(orderRead);
-                }
+                orderRead = new Order(orders.getInt("ID") ,ordersLines ,date ,sent ,cancel);
+
+                ordersNotShipped.add(orderRead);
 
             }
 
@@ -186,6 +249,93 @@ public class DatabaseManagement {
     }
 
 
+    public ArrayList<OrderLine> getOrderLineByIDOrder(int IDOrder, Connection connection){
+        ArrayList<OrderLine> ordersLines = new ArrayList<>();
+
+        Statement sentence = null;
+        ResultSet ordersLinesRead = null;
+        OrderLine orderLineRead;
+        Product productRead;
+        int quantity;
+
+        String select = "SELECT IDOrder, IDProduct, Quantity " +
+                        "FROM OrdersLines " +
+                        "WHERE IDOrder = "+IDOrder;
+
+        try {
+
+            sentence = connection.createStatement();
+            ordersLinesRead = sentence.executeQuery(select);
+            while (ordersLinesRead.next()){
+
+                productRead = getProductByID(ordersLinesRead.getInt("IDProduct"), connection);
+
+                quantity = ordersLinesRead.getInt("Quantity");
+
+                orderLineRead = new OrderLine(productRead,quantity);
+
+                ordersLines.add(orderLineRead);
+
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        finally {
+            try {
+                ordersLinesRead.close();
+                sentence.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+
+        return ordersLines;
+    }
+
+
+    /**
+     * @param IDProduct
+     * @param connection
+     * @return
+     */
+
+
+    public Product getProductByID(int IDProduct, Connection connection){
+        Product productReturn = null;
+
+        Statement sentence = null;
+        ResultSet products = null;
+
+        String select = "SELECT ID, Name, Characteristics ,Price " +
+                        "FROM Products " +
+                        "WHERE ID = "+IDProduct;
+
+        try {
+            sentence = connection.createStatement();
+            products = sentence.executeQuery(select);
+            while (products.next()){
+                productReturn = new Product(products.getInt("ID"),products.getString("Name"),products.getString("Characteristics"),products.getInt("Price"));
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        finally {
+            try {
+                products.close();
+                sentence.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+
+        return productReturn;
+    }
+
+
     /**
      * @param order
      * @param orderLine
@@ -200,7 +350,7 @@ public class DatabaseManagement {
         Statement sentence = null;
 
         String select = "INSERT INTO OrdersLines(IDOrder,IDProduct,Quantity) " +
-                        "VALUES("+order.getID()+", "+orderLine.getIDProduct()+", "+orderLine.getProductQuantity()+")";
+                "VALUES("+order.getID()+", "+orderLine.getIDProduct()+", "+orderLine.getProductQuantity()+")";
 
         try {
             sentence = connection.createStatement();
@@ -219,6 +369,48 @@ public class DatabaseManagement {
 
         return succes;
     }
+
+
+    /**
+     * @param order
+     * @param IDProductToDelete
+     * @param connection
+     * @return
+     */
+
+
+    public boolean deleteOrderLine(Order order, int IDProductToDelete, Connection connection){
+
+        boolean succes = false;
+        Statement sentence = null;
+
+        String delete = "DELETE FROM OrdersLines " +
+                        "WHERE IDOrder = "+order.getID()+" AND IDProduct ="+IDProductToDelete;
+
+        try {
+            sentence = connection.createStatement();
+            succes = (sentence.executeUpdate(delete) > 0);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        finally {
+            try {
+                sentence.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+
+        return succes;
+    }
+
+
+    /**
+     * @param DNI
+     * @param connection
+     * @return
+     */
 
 
     public Employee getEmployeeByDNI(String DNI, Connection connection){
@@ -267,6 +459,14 @@ public class DatabaseManagement {
     }
 
 
+    /**
+     * @param schedule
+     * @param DNI
+     * @param connection
+     * @return
+     */
+
+
     public boolean insertSchedule(Schedule schedule, String DNI, Connection connection){
 
         boolean succes = false;
@@ -289,12 +489,12 @@ public class DatabaseManagement {
 
         endDate = month+"/"+day+"/"+year;
 
-        String select = "INSERT INTO Schedules (DNIEmployee, [WeekDay], StartDate, EndDate) " +
+        String insert = "INSERT INTO Schedules (DNIEmployee, [WeekDay], StartDate, EndDate) " +
                         "VALUES ('"+DNI+"','"+schedule.getWeekDay()+"','"+startDate+"','"+endDate+"')";
 
         try {
             sentence = connection.createStatement();
-            succes = (sentence.executeUpdate(select) > 0);
+            succes = (sentence.executeUpdate(insert) > 0);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
@@ -310,6 +510,14 @@ public class DatabaseManagement {
         return succes;
     }
 
+    /**
+     * @param schedule
+     * @param DNI
+     * @param connection
+     * @return
+     */
+
+
     public boolean insertSchedules(Schedule[] schedule, String DNI, Connection connection){
 
         boolean succes = false;
@@ -318,7 +526,7 @@ public class DatabaseManagement {
         int day, month, year;
         GregorianCalendar date;
 
-        String select = "INSERT INTO Schedules (DNIEmployee, [WeekDay], StartDate, EndDate) VALUES";
+        String insert = "INSERT INTO Schedules (DNIEmployee, [WeekDay], StartDate, EndDate) VALUES";
 
         for (int i = 0; i < schedule.length; i++){
 
@@ -337,15 +545,15 @@ public class DatabaseManagement {
             endDate = month+"/"+day+"/"+year;
 
             if (i == schedule.length-1){
-                select+= " ('"+DNI+"','"+schedule[i].getWeekDay()+"','"+startDate+"','"+endDate+"')";
+                insert+= " ('"+DNI+"','"+schedule[i].getWeekDay()+"','"+startDate+"','"+endDate+"')";
             }else{
-                select+= " ('"+DNI+"','"+schedule[i].getWeekDay()+"','"+startDate+"','"+endDate+"'),";
+                insert+= " ('"+DNI+"','"+schedule[i].getWeekDay()+"','"+startDate+"','"+endDate+"'),";
             }
         }
 
         try {
             sentence = connection.createStatement();
-            succes = (sentence.executeUpdate(select) > 0);
+            succes = (sentence.executeUpdate(insert) > 0);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
@@ -360,6 +568,13 @@ public class DatabaseManagement {
 
         return succes;
     }
+
+
+    /**
+     * @param payslip
+     * @param connection
+     * @return
+     */
 
 
     public boolean modifySalary(Payslip payslip, Connection connection){
@@ -385,6 +600,153 @@ public class DatabaseManagement {
         }
 
         return succes;
+    }
+
+    public boolean decreaseAmountProductOrderLine(Order orderChoosed, int IDProductToDecrease, int amountToDecrease, Connection connection){
+
+        boolean succes = false;
+        Statement sentence = null;
+
+        String update = "UPDATE OrdersLines " +
+                        "SET Quantity -= "+amountToDecrease +
+                        " WHERE IDOrder = "+orderChoosed.getID()+" AND IDProduct = "+IDProductToDecrease;
+
+        try {
+            sentence = connection.createStatement();
+            succes = (sentence.executeUpdate(update) > 0);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        try {
+            sentence.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return succes;
+    }
+
+
+    /**
+     * @param orderChoosed
+     * @param IDProductToDecrease
+     * @param amountToDecrease
+     * @param connection
+     * @return
+     */
+
+
+    public boolean increaseAmountProductOrderLine(Order orderChoosed, int IDProductToDecrease, int amountToDecrease, Connection connection){
+
+        boolean succes = false;
+        Statement sentence = null;
+
+        String update = "UPDATE OrdersLines " +
+                        "SET Quantity += "+amountToDecrease +
+                        " WHERE IDOrder = "+orderChoosed.getID()+" AND IDProduct = "+IDProductToDecrease;
+
+        try {
+            sentence = connection.createStatement();
+            succes = (sentence.executeUpdate(update) > 0);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        try {
+            sentence.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return succes;
+    }
+
+
+    /**
+     * @param orderChoosed
+     * @param connection
+     * @return
+     */
+
+
+    public boolean markCancelProduct(Order orderChoosed, Connection connection){
+
+        boolean succes = false;
+        Statement sentence = null;
+
+        String update = "UPDATE Orders " +
+                        "SET Cancel = 1 " +
+                        "WHERE ID = "+orderChoosed.getID();
+
+        try {
+            sentence = connection.createStatement();
+            succes = (sentence.executeUpdate(update) > 0);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        try {
+            sentence.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return succes;
+    }
+
+
+    public ArrayList<Schedule> getEmployeeSchedules(String DNIEmployee, Connection connection){
+
+        ArrayList<Schedule> schedules = new ArrayList<>();
+
+        Statement sentence = null;
+        ResultSet schedulesRead = null;
+
+        String lineDate, weekDay;
+        int day ,month ,year;
+        GregorianCalendar startDate;
+        GregorianCalendar endDate;
+
+        String select = "SELECT WeekDay, StartDate, EndDate " +
+                        "FROM Schedules " +
+                        "WHERE YEAR(StartDate) = YEAR(CURRENT_TIMESTAMP) " +
+                        "AND MONTH(StartDate) = MONTH(CURRENT_TIMESTAMP) " +
+                        "AND DNIEmployee = '"+DNIEmployee+"'";
+
+        try {
+            sentence = connection.createStatement();
+            schedulesRead = sentence.executeQuery(select);
+            while (schedulesRead.next()){
+
+                lineDate = schedulesRead.getString("StartDate");
+                day = Integer.parseInt(lineDate.split("-")[2].substring(0,2));
+                month =Integer.parseInt(lineDate.split("-")[1]);
+                year = Integer.parseInt(lineDate.split("-")[0]);
+                startDate = new GregorianCalendar(year,month,day);
+
+                lineDate = schedulesRead.getString("EndDate");
+                day = Integer.parseInt(lineDate.split("-")[2].substring(0,2));
+                month =Integer.parseInt(lineDate.split("-")[1]);
+                year = Integer.parseInt(lineDate.split("-")[0]);
+                endDate = new GregorianCalendar(year,month,day);
+
+                weekDay = schedulesRead.getString("WeekDay");
+
+                schedules.add(new Schedule(EnumWeekDays.valueOf(weekDay), startDate, endDate));
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        try {
+            sentence.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return schedules;
     }
 
 }
