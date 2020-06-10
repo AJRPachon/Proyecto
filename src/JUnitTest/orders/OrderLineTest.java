cca4021e4908f2c15d1b5f66c351d1a8953ffd78
AJRPachon <49155621+AJRPachon@users.noreply.github.com>
2020-06-10 17:00:31 +0200

    test

--- src/JUnitTest/orders/OrderLineTest.java
+++ src/JUnitTest/orders/OrderLineTest.java
@@ -18,35 +18,52 @@ class OrderLineTest {
     @Test
     void getNameProduct() {
         String name = ol.getNameProduct();
-        assertNotEquals(name, "anchoas");
+        assertNotEquals(name, "McFlurry");
     }
 
     @Test
     void getCharacteristicsProduct() {
+        String characteristics = ol.getCharacteristicsProduct();
+        assertNotEquals(characteristics, "not default");
     }
 
     @Test
     void getPriceProduct() {
+        double price = ol.getPriceProduct();
+        assertEquals(price, 0.0);
     }
 
     @Test
     void getProductQuantity() {
+        int productQuantity = ol.getProductQuantity();
+        assertNotEquals(productQuantity, -1);
     }
 
     @Test
     void setProductQuantity() {
+        ol.setProductQuantity(40);
+        int productQuantity = ol.getProductQuantity();
+        assertEquals(productQuantity, 40);
     }
 
     @Test
     void increaseQuantity() {
+        ol.increaseQuantity(20);
+        int incQuantity = ol.getProductQuantity();
+        assertNotEquals(incQuantity, 0);
     }
 
     @Test
     void decreaseQuantity() {
+        ol.decreaseQuantity(1);
+        int decQuantity = ol.getProductQuantity();
+        assertNotEquals(decQuantity, -1);
     }
 
     @Test
     void getTotalPrice() {
+        double totalPrice = ol.getTotalPrice();
+        assertNotEquals(totalPrice, -1);
     }
 
     @Test
